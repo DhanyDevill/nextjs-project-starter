aa85619fe50165220c05ffa3d9ecb399707a8de7
DEVILL <dhany.gcloud@gmail.com>
2025-07-15 21:39:45 +0800

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1752586786507

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_29e89fff._.js",
+  "static/chunks/src_context_language-context_tsx_9056a013._.js"
+],
+    source: "dynamic"
+});
